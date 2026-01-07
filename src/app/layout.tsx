2d2232b72bdd4e15219224aeb7d9e141627a0c12
DemianMH <za19011250@zapopan.tecmm.edu.mx>
2026-01-07 10:34:55 -0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="scroll-smooth">
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="FastPack" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className="antialiased bg-slate-50">
+        
         <Navbar />
         {children}
         <Footer />
